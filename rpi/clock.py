image scaling and fixed colors

rpi/clock.py
@@ -1,15 +1,16 @@
-from datetime import datetime
-import random
+import math
 import mmap
-import sys
 import os
-import time
-import numpy as np
-from PIL import Image, ImageDraw, ImageFont
+import random
+from datetime import datetime
 from glob import glob
 
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+from argparse import ArgumentParser
 
 FB_DEV = "/dev/fb0"
+IMG_PATH = "/home/butthead/clock_images/*.jpg"
 
 with open(f"/sys/class/graphics/{os.path.basename(FB_DEV)}/virtual_size", "r") as f:
     width_str, height_str = f.read().strip().split(",")
@@ -69,15 +70,40 @@ def getsize(font, text):
 
 
 def blit_img(fname):
-    img = Image.open(fname)
-    if img.mode != "RGBA":
-        img = img.convert("RGBA")
-    if img.size[0] > WIDTH or img.size[1] > HEIGHT:
-        raise Exception(
-            f"image dimensions {r_chan.size} exceed screen ({WIDTH}, {HEIGHT})"
-        )
-    elif img.size[0] < WIDTH or img.size[1] < HEIGHT:
-        img = img.resize((WIDTH, HEIGHT), Image.Resampling.LANCZOS)
+    if fname is None:
+        print("test pattern")
+        src = Image.new("RGBA", (WIDTH, HEIGHT), (0, 0, 0))
+        draw = ImageDraw.Draw(src)
+        draw.rectangle([0, 0, WIDTH // 3, HEIGHT], fill="red")
+        draw.rectangle([WIDTH // 3, 0, 2 * WIDTH // 3, HEIGHT], fill="green")
+        draw.rectangle([2 * WIDTH // 3, 0, WIDTH, HEIGHT], fill="blue")
+    else:
+        print(f"loading {fname}")
+        src = Image.open(fname)
+
+    if src.mode != "RGBA":
+        src = src.convert("RGBA")
+    if src.size == (WIDTH, HEIGHT):
+        img = src
+    else:
+        if src.size[0] / src.size[1] == WIDTH / HEIGHT:
+            img = src.resize((WIDTH, HEIGHT), Image.Resampling.LANCZOS)
+        else:
+            img = Image.new("RGBA", (WIDTH, HEIGHT), (0, 0, 0))
+            if src.size[0] / src.size[1] > WIDTH / HEIGHT:
+                # image is wider than our display
+                new_h = math.floor(WIDTH * src.size[1] / src.size[0])
+                img.paste(
+                    src.resize((WIDTH, new_h), Image.Resampling.LANCZOS),
+                    (0, (HEIGHT - new_h) // 2),
+                )
+            else:
+                # image is taller than our display
+                new_w = math.floor(HEIGHT * src.size[0] / src.size[1])
+                img.paste(
+                    src.resize((new_w, HEIGHT), Image.Resampling.LANCZOS),
+                    ((WIDTH - new_w) // 2, 0),
+                )
 
     draw = ImageDraw.Draw(img)
     time = f"{datetime.now():%I:%M %p}"
@@ -92,8 +118,7 @@ def blit_img(fname):
 
     if BPP == 32:
         fb = np.memmap(FB_DEV, dtype="uint8", mode="w+", shape=(HEIGHT, WIDTH, 4))
-        fb[:] = np.asarray(img)
-
+        fb[:, :, [2, 1, 0, 3]] = np.asarray(img)
     elif BPP == 16:
         fb = np.memmap(FB_DEV, dtype="uint16", mode="w+", shape=(HEIGHT, WIDTH))
         arr = np.asarray(img, dtype=np.uint8)  # shape (H, W, 3)
@@ -105,5 +130,13 @@ def blit_img(fname):
 
 
 if __name__ == "__main__":
-    # test_img()
-    blit_img(random.choice(glob("/home/butthead/clock_images/*.jpg")))
+    parser = ArgumentParser()
+    parser.add_argument("--img")
+    parser.add_argument("--test-patt", action="store_true")
+    args = parser.parse_args()
+    if args.test_patt:
+        blit_img(None)
+    elif args.img:
+        blit_img(args.img)
+    else:
+        blit_img(random.choice(glob(IMG_PATH)))